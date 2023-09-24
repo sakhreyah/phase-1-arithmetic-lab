463814712d6c85afcaab7dea88fbe38e37c43aaf
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-09-24 22:59:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+const num3 = 14, num4 = 10;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 20);
